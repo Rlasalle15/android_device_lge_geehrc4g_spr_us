ef6efba7fdc0d1e5c620377374302ea4feb2847f
Ryan La Salle <ryanlasalle@gmail.com>
2013-04-17 15:06:04 -0400

    Switch to new kernel
    Change-Id: I2386a96dc38d3086fea00cecf0891d730a7901fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 #TARGET_KERNEL_SOURCE := kernel/lge/geem
 
 TARGET_KERNEL_CONFIG := geehrc_defconfig
-TARGET_KERNEL_SOURCE := kernel/lge/geem
+TARGET_KERNEL_SOURCE := kernel/lge/gee_mako
 
 # inherit from gee-common
 -include device/lge/gee-common/BoardConfigCommon.mk
