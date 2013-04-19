0d2dc80dcfd057b0be65ceebd2d6da2daa4f5c77
Ryan La Salle <ryanlasalle@gmail.com>
2013-04-19 01:49:52 -0400

    Move Auto Focus and Camera permissions to Common
    Change-Id: I1ebf18ccdef7863c81af2cc110c424a19339173a

--- device.mk
+++ device.mk
@@ -72,8 +72,6 @@ PRODUCT_COPY_FILES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # CameraHAL
