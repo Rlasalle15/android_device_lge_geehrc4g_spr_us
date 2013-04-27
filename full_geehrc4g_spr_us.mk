693d4381ff5a7575fe808fac74dcbc2a736b2572
Ryan La Salle <ryanlasalle@gmail.com>
2013-04-27 15:36:27 -0400

    Remove since these are called in RootBOX vendor

--- full_geehrc4g_spr_us.mk
+++ full_geehrc4g_spr_us.mk
@@ -30,11 +30,5 @@ PRODUCT_MODEL := LG-LS970
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Boot Animation
-BOOTANIMATION_RESOLUTION := 720x1280
-
-#Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc4g_spr_us BUILD_FINGERPRINT=lge/geehrc4g_spr_us/geehrc4g:4.1.2/JZO54K/LS970ZVB.1360739022:user/release-keys PRIVATE_BUILD_DESC="geehrc4g_spr_us-user 4.1.2 JZO54K LS970ZVB.1360739022 release-keys"
-
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/geehrc4g_spr_us/device.mk)
